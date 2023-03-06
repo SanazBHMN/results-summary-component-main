16b7933c1b1e8c92a24d557bd70d9868ed87089b
SanazBHMN <sanazbahmani35@gmail.com>
2023-03-06 12:48:20 +0330

    Dynamically display summary data

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
-fetch("data.json")
+const url = "./data.json";
+const textColors = [
+  "text-light-red",
+  "text-orangey-yellow",
+  "text-green-teal",
+  "text-cobalt-blue",
+];
+const backgroundColors = [
+  "bg-red-transparent",
+  "bg-orange-transparent",
+  "bg-green-transparent",
+  "bg-blue-transparent",
+];
+let parsedData;
+const listItems = document.querySelectorAll("li");
+
+fetch(url)
   .then((response) => response.json())
-  .then((data) => console.log(data));
+  .then((data) => {
+    parsedData = data;
+    console.log(parsedData);
+    listItems.forEach((item, index) => {
+      console.log(item, index);
+      item.innerHTML = `
+        <div class="flex justify-center items-center">
+          <img src=${parsedData[index].icon} class="inline mr-2" alt="icon" />
+          <span class="font-semibold ${textColors[index]}">${parsedData[index].category}</span>
+        </div>
+        <div class="font-semibold">
+          <span>${parsedData[index].score}</span>
+          <span class="text-gray-transparent">/ 100</span>
+        </div>
+      `;
+      item.classList.add(backgroundColors[index]);
+    });
+  });
